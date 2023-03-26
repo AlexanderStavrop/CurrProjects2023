51886d8e808e8025ba4361521cf3edf9eddb9ef5
Alexandros Stavropoulos <alexanderstavrop@gmail.com>
2023-03-26 07:45:09 +0300

    push push

--- Reinforcement Learning/Exercise_1/epsilonGreedy.py
+++ Reinforcement Learning/Exercise_1/epsilonGreedy.py
@@ -12,6 +12,7 @@ class Tester:
         # Creating an array of individual dictionaries for each bandit
         self.bandits = np.array([{'prob': 0, 'reward': 0} for _ in range(self.K)])
         self.best_bandit = {'arm': 0,  "arm_score": 0, 'round_score': [0 for _ in range(self.T)]}
+        print(self.best_bandit['arm_score'])
 
         # Creating the bandits
         self.create_bandits()
@@ -100,6 +101,7 @@ class EpsilonGreedy:
         self.T = rounds
         # self.bandits = bandits
         self.best_bandit = best_bandit
+        print(self.best_bandit['arm_score'])
 
 
         # Creating two dictionaries containing the needed values for measuring the performance of the algorithm
@@ -163,7 +165,7 @@ class EpsilonGreedy:
         for curr_round in range(self.T):
 
             # Updating the epsilon value
-            epsilon = (1 + curr_round)**(-1/3) * (np.log10(1 + curr_round))**(1/3)
+            epsilon = (1 + curr_round)**(-1/3) * (self.K * np.log10(1 + curr_round))**(1/3)
 
             # Choosing the arm
             arm = self.choose_action(epsilon)
@@ -177,17 +179,21 @@ class EpsilonGreedy:
         return self.algo_stats['regret']
 
 ###################################################################### UCB ######################################################################
-# Creating a class for espilonGreedy algorithm
+# Creating a class for UCB algorithm
 class UCB:
     def __init__(self, numOfBandits, rounds, bandits, best_bandit):
         self.K = numOfBandits
         self.T = rounds
         self.best_bandit = best_bandit
+        for i in range(self.T):
+            self.best_bandit['round_score'][i] = 0
+
+        # self.best_bandit['round_score'][x] for x in range(self.T)
+        # self.best_bandit = np.array([{'prob': best_bandit[x]['prob'], 'reward': best_bandit[x]['reward'], 'score':0} for x in range(self.K)])
 
         # Creating two dictionaries containing the needed values for measuring the performance of the algorithm
         self.bandits = np.array([{'prob': bandits[x]['prob'], 'reward': bandits[x]['reward'], 'score':0, 'Q':0, 'mu':0, 'ucb':2**10} for x in range(self.K)])
         self.algo_stats = {'round_score': [0 for _ in range(self.T)], 'algo_score': [0 for _ in range(self.T)], 'regret': [0 for _ in range(self.T)]}
-
 
     # Function for updating the measurements
     def update_measurements(self, arm, round):
@@ -253,11 +259,11 @@ if __name__ == '__main__':
     # Create bandits
     tester = Tester(K, T)
 
-    # Running the epsilon greedy algorithm
-    epsilon_regret = tester.run_EpsilonGreedy()
-
     # Running the UCB algorithm
     ucb_regret = tester.run_UCB()
+
+    # Running the epsilon greedy algorithm
+    epsilon_regret = tester.run_EpsilonGreedy()
 
     # Plotting the results
     tester.plot_regret(epsilon_regret, 'Epsilon Greedy', ucb_regret, 'UCB')
