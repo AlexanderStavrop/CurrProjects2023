be829e3e89bbc25583005edb0691aa9c21620768
alexanderstavrop <alexanderstavrop@gmail.com>
2023-04-16 14:53:48 +0300

    push push

--- PowerElectronics/Exericise 2/code/PowerElectronics_2.m
+++ PowerElectronics/Exericise 2/code/PowerElectronics_2.m
@@ -2,174 +2,207 @@ clc;
 clear;
 close all;
 
-%% Question 1
-Vrms=230;
-f=50;
-dt=0.00002;
-T=10;
+%% Exercise data
 
-t=0:dt:T;
-t_period = 0:dt:1/f;
+% 
+Vrms = 230;
+f = 50;
+R = 2.5;
 
+Time = 10;
+dt = 0.00002;
+
+% Creating a vector for represent the total time with time step dt
+t_total = 0:dt:Time;
+
+% Creating a vector for represent the time of two periods with time step dt
+t_periods = 0:dt:2/f;
+% Creating a vector for represent the time until 0.4 secs with time step
+% dt for representing the output current
+t_I = 0:dt:0.4;
+
+% Calculating the value of omega
 omega = 2*pi*f;
-
-V_in = Vrms*sqrt(2) * sin(omega*t);
-R=2.5;
 
 
 %% Question 2
-a = [0 deg2rad(90)];
-L = [0.04 0.08];
 
-inp_struct = {'V_in', V_in};
+% Creating the input voltage for the single phase
+V_i = Vrms*sqrt(2) * sin(omega*t_total);
 
-for a_i = [a(1) a(2)]       
-    for L_i = [L(1) L(2)]
-        % Initializing the needed vectors to zero
-        V_out = zeros(1, length(t));
-        I_out = zeros(1, length(t));
-        x_sys = zeros(1, length(t)+1); 
-        
-        %Initializing thyristor vectors
-        T1 = zeros(1, length(t));       
-        T2 = zeros(1, length(t));       
-        T3 = zeros(1, length(t));      
-        T4 = zeros(1, length(t));  
 
-        % Calculating the system parameters
-        A=-R/L_i;
-        B=1/L_i;
-        C=1;
-        D=0;
-        
-        % Creating the continuous and discrete system
-        sys = ss(A, B, C, D); 
-        sysd = c2d(sys, dt);
-        
-        % Extracting the variables????
-        A_d = sysd.A;
-        B_d = sysd.B;
-        C_d = sysd.C;
-        D_d = sysd.D;
-        
-        for i = 1:length(t)
-            % Calculating the current phase value restricted in [0 2*pi]
-            phase = mod(omega*t(i), 2*pi);
-            
-            % If the phase is in the terretory [a, a+pi]
-            if(phase >= a_i && phase < a_i+ pi)
-                % Using the positive input Voltage
-                V_out(i) = V_in(i);
-                T1(i)=1;
-                T2(i)=1;
-                T3(i)=0;
-                T4(i)=0;
-            % Else
-            else
-                % Using the negative input voltage
-                V_out(i) = -V_in(i);
-                T1(i)=0;
-                T2(i)=0;
-                T3(i)=1;
-                T4(i)=1;
-            end
-            
-            % Solving the system parameters
-            x_sys(i+1) = A_d*x_sys(i) + B_d*V_out(i);
-            I_out(i)   = C_d*x_sys(i) + D_d*V_out(i);
-
-            if(x_sys(i+1) <= 0)
-                % Setting the needed variables to zero
-                x_sys(i+1) = 0;
-                V_out(i) = 0;
-                
-                % Recalculating the current
-                I_out(i) = C_d*x_sys(i) + D_d*V_out(i);
-            end            
-        end
-        Thyristors_currs = {T1.*I_out T2.*I_out T3.*I_out T4.*I_out T4.*I_out T4.*I_out};
-        plotter(inp_struct, V_out, Thyristors_currs, t_period, a, L)
-    end
-end
+% a = [0 deg2rad(90)];
+% L = [0.04 0.08];
+% 
+% inp_struct = {'V_in', V_in};
+% 
+% for a_i = [a(1) a(2)]       
+%     for L_i = [L(1) L(2)]
+%         % Initializing the needed vectors to zero
+%         V_out = zeros(1, length(t));
+%         I_out = zeros(1, length(t));
+%         x_sys = zeros(1, length(t)+1); 
+% 
+%         %Initializing thyristor vectors
+%         T1 = zeros(1, length(t));       
+%         T2 = zeros(1, length(t));       
+%         T3 = zeros(1, length(t));      
+%         T4 = zeros(1, length(t));  
+% 
+%         % Calculating the system parameters
+%         A=-R/L_i;
+%         B=1/L_i;
+%         C=1;
+%         D=0;
+% 
+%         % Creating the continuous and discrete system
+%         sys = ss(A, B, C, D); 
+%         sysd = c2d(sys, dt);
+% 
+%         % Extracting the variables????
+%         A_d = sysd.A;
+%         B_d = sysd.B;
+%         C_d = sysd.C;
+%         D_d = sysd.D;
+% 
+%         for i = 1:length(t)
+%             % Calculating the current phase value restricted in [0 2*pi]
+%             phase = mod(omega*t(i), 2*pi);
+% 
+%             % If the phase is in the terretory [a, a+pi]
+%             if(phase >= a_i && phase < a_i+ pi)
+%                 % Using the positive input Voltage
+%                 V_out(i) = V_in(i);
+%                 T1(i)=1;
+%                 T2(i)=1;
+%             % Else
+%             else
+%                 % Using the negative input voltage
+%                 V_out(i) = -V_in(i);
+%                 T3(i)=1;
+%                 T4(i)=1;
+%             end
+% 
+%             % Solving the system parameters
+%             x_sys(i+1) = A_d*x_sys(i) + B_d*V_out(i);
+%             I_out(i)   = C_d*x_sys(i) + D_d*V_out(i);
+% 
+%             if(x_sys(i+1) <= 0)
+%                 % Setting the needed variables to zero
+%                 x_sys(i+1) = 0;
+%                 V_out(i) = 0;
+% 
+%                 % Recalculating the current
+%                 I_out(i) = C_d*x_sys(i) + D_d*V_out(i);
+%             end            
+%         end
+%         Thyristors_currs = {T1.*I_out T2.*I_out T3.*I_out T4.*I_out T4.*I_out T4.*I_out};
+%         plotter(inp_struct, V_out, Thyristors_currs, t_period, a, L)
+%     end
+% end
 
 %% Question 3
 
-V_ab = Vrms*sqrt(2)*sqrt(3) * sin(omega*t);
-V_bc = Vrms*sqrt(2)*sqrt(3) * sin(omega*t + deg2rad(120));
-V_ca = Vrms*sqrt(2)*sqrt(3) * sin(omega*t + deg2rad(240));
+% Creating the three voltage inputs 
+V_ab = Vrms*sqrt(2)*sqrt(3) * sin(omega*t_total);
+V_bc = Vrms*sqrt(2)*sqrt(3) * sin(omega*t_total + deg2rad(120));
+V_ca = Vrms*sqrt(2)*sqrt(3) * sin(omega*t_total + deg2rad(240));
 
-inp_struct = {'V_ab', V_ab, 'V_bc', V_bc, 'V_ca', V_ca};
+% THIS IS FOR PRINTING, IGNORE
+inp_struct = {'V_ab', V_ab, 'V_bc', V_bc, 'V_ac', V_ca};
 
-% period=1/50;
-% 
-% set(gca,'XTick',0:period/4:2*period) 
-% set(gca,'XTickLabel',{'0','\pi/2','\pi','3*\pi/2','2*\pi','5*\pi/2','3*\pi','7*\pi/2','4*\pi'})
-
-
-
+% Creating tow vectors containing the values of a and L
 a = [0 deg2rad(67)];
 L = [0.04 0.08];
 
+% For each a
 for a_i = [a(1) a(2)]
+    % for each L
     for L_i = [L(1) L(2)]
         % Initializing the needed vectors to zero
-        V_out = zeros(1, length(t));
-        I_out = zeros(1, length(t));
-        x_sys = zeros(1, length(t)+1); 
+        V_out = zeros(1, length(t_total));
+        I_out = zeros(1, length(t_total));
+        x_sys = zeros(1, length(t_total)+1);
 
         %Initializing thyristor vectors
-        T1 = zeros(1, length(t));       
-        T2 = zeros(1, length(t));       
-        T3 = zeros(1, length(t));      
-        T4 = zeros(1, length(t));  
-        T5 = zeros(1, length(t));  
-        T6 = zeros(1, length(t));  
+        T1 = zeros(1, length(t_total));       
+        T2 = zeros(1, length(t_total));       
+        T3 = zeros(1, length(t_total));      
+        T4 = zeros(1, length(t_total));  
+        T5 = zeros(1, length(t_total));  
+        T6 = zeros(1, length(t_total));  
         
-        A=-R/L_i;
-        B=1/L_i;
-        C=1;
-        D=0;
+        % Calculating the system variables
+        A = -R/L_i;
+        B = 1/L_i;
+        C = 1;
+        D = 0;
 
         % Creating the continuous and discrete system
         sys = ss(A, B, C, D); 
         sysd = c2d(sys, dt);
         
-        % Extracting the variables????
-        A_d = sysd.A;
+        % Extracting the system variables
+        A_d = sysd.A;   
         B_d = sysd.B;
         C_d = sysd.C;
         D_d = sysd.D;
 
-        for i = 1:length(t)
+        % For each time step
+        for i = 1:length(t_total)
             % Calculating the current phase value restricted in [0 2*pi]
-            phase = mod(omega*t(i), 2*pi); 
-%             bool = (phase < pi);
-%             not_bool = (phase >= pi);
-%             if(a_i + not_bool*pi <= phase && phase < a_i + pi/3 + not_bool*pi)
-%                 V_out(i) = -bool*2*V_ca(i) + V_ca(i);
-%             
-%             elseif(a_i + pi/3 + not_bool*pi <= phase && phase < a_i + 2*pi/3 + not_bool*pi)           
-%                 V_out(i) = -not_bool*2*V_ab(i) + V_ab(i);
-% 
-%             elseif(a_i + 2*pi/3 + not_bool*pi <= phase && phase < a_i + pi + not_bool*pi)             
-%                 V_out(i) = -bool*2*V_bc(i) + V_bc(i);
-%             end
+            phase = mod(omega*t_total(i), 2*pi);
 
-            if(a_i <= phase && phase < a_i + pi/3)
-                V_out(i) = (-1 * (phase < pi))*2*V_ca(i) + V_ca(i);
-            elseif(phase >= a_i + pi/3 && phase < a_i + 2*pi/3)
+            % According to the value of a and the value of phase the output
+            % voltgage signal is created according to the thyristors that 
+            % are on
+
+            if(a_i < phase && phase <= a_i + pi/3)
+                V_out(i) = -V_ca(i);
+                T1(i) = 1;
+                T2(i) = 1;
+            elseif(a_i + pi/3 < phase && phase <= a_i + 2*pi/3)
                 V_out(i) = V_ab(i);
-            elseif(phase >= a_i + 2*pi/3 && phase < a_i + pi)
-                V_out(i) = -V_bc(i);
-            elseif(phase >= a_i + pi && phase < a_i + 4*pi/3)
+                T1(i) = 1;
+                T6(i) = 1;
+            elseif(a_i + 2*pi/3 < phase && phase <= a_i + pi)
+                V_out(i) = -V_bc(i);  
+                T5(i) = 1;
+                T6(i) = 1;
+            elseif(a_i + pi <= phase && phase <= a_i + 4*pi/3)
                 V_out(i) = V_ca(i);
-            elseif(phase >= a_i + 4*pi/3 && phase < a_i + 5*pi/3)
+                T4(i) = 1;
+                T5(i) = 1;
+            elseif(a_i + 4*pi/3 <= phase  && phase <= a_i + 5*pi/3 || a_i > pi/3 && phase <= mod(a_i + 5*pi/3, 2*pi))
                 V_out(i) = -V_ab(i);
+                T3(i) = 1;
+                T4(i) = 1;
             else
                 V_out(i) = V_bc(i);
+                T2(i) = 1;
+                T3(i) = 1;
             end
+
+            % Solving the system parameters
+            x_sys(i+1)  = A_d*x_sys(i)  + B_d*V_out(i);
+            I_out(i)  = C_d*x_sys(i)  + D_d*V_out(i);
+                
+            if(x_sys(i+1) <= 0)
+                % Setting the needed variables to zero
+                x_sys(i+1) = 0;
+                V_out(i) = 0;  
+                
+                % Recalculating the current
+                I_out(i) = C_d*x_sys(i) + D_d*V_out(i);
+            end        
         end
-        
-        plotter(inp_struct, V_out, '0', t_period, a, L)
+    % Plotting the signals
+    Thyristors = {T1.*I_out T2.*I_out T3.*I_out T4.*I_out T4.*I_out T4.*I_out};
+    plotter(inp_struct, V_out, I_out, Thyristors, t_I, t_periods, a_i, L_i)
     end
 end
+
+%% Question 4
+
+
+
--- PowerElectronics/Exericise 2/code/plotter.m
+++ PowerElectronics/Exericise 2/code/plotter.m
@@ -1,40 +1,72 @@
-function plotter(volts, vout, thyristors, t_period, a, L)
-    
+function plotter(volts, vout, iout, thyristors, t_I, t_periods, a, L)
+    path = '~/Downloads/Exercise_2/Images/';
     colors = ["cyan" "red" "green" "black"];
-    
+
+    %%
+    % ΝΑ ΦΤΙΑΞΩ ΤΟΥΣ ΑΞΟΝΕΣ ΚΑΙ ΤΑ ΟΝΟΜΑΤΑ ΣΤΑ ΡΕΥΜΑΤΑ !!!!!!!!!!!!!!!!!!!!
+    %%
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    fig = figure('Renderer', 'painters','Name','V_in vs V_out','NumberTitle','off', 'Position', [10 10 1300 600] );
+    fig = figure('Renderer', 'painters','Name','V_in vs V_out','NumberTitle','off', 'Position', [10 10 900 540] );
     title(sprintf('V_{out} vs V_{in} (a=%s deg, L=%.2f H)', num2str(a*180/pi), L))
     set(0,'DefaultLineLineWidth',1.2)
-    
+
     for i = 1:2:length(volts)
         name = volts{i};
+        name = sprintf('%s{%s}', name(1:2), name(3:end));
         signal = volts{i + 1};
 
-        plot(t_period, signal(1:length(t_period)), 'color', colors(ceil(i/2)), 'DisplayName',name)
+        plot(t_periods, signal(1:length(t_periods)), 'color', colors(ceil(i/2)), 'DisplayName',name)
         hold on;
-        plot(t_period, -signal(1:length(t_period)), 'color', colors(ceil(i/2)), 'LineStyle', '--')
+        name = sprintf("-%s", name);
+        plot(t_periods, -signal(1:length(t_periods)), 'color', colors(ceil(i/2)), 'LineStyle', '--', 'DisplayName',name)
         hold on;
     end
-    
-    plot(t_period, vout(1:length(t_period)), 'color', colors(4), "LineStyle"','--')
+
+    plot(t_periods, vout(1:length(t_periods)), 'color', colors(4), "LineStyle"','--', 'DisplayName','V_{out}')
     legend()
+
+    fname = sprintf('%s3_Vout_%s_%s',path, num2str(a*180/pi), sprintf('%02d', L*100));
+    print(fname, '-depsc')
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    fig = figure('Renderer', 'painters','Name','I_out','NumberTitle','off', 'Position', [10 10 900 540]);
+    title(sprintf('I_{out} (a=%s deg, L=%.2f H)', num2str(a*180/pi), L))
+
+    plot(t_I, iout(1:length(t_I)), 'DisplayName','I_{out}')
+    legend()
+
+    xstart=.4;
+    xend=0.87;
+    ystart=.2;
+    yend=.5;
+    axes('position',[xstart ystart xend-xstart yend-ystart ])
+    box on
+
+    start_indx = find(t_I<=0.28, 1, 'last');
+    end_indx = find(t_I<=0.32, 1, 'last');
+
+    plot(t_I(start_indx:end_indx), iout((start_indx:end_indx)), 'DisplayName','I_{out}')
+    fname = sprintf('%s3_Iout_%s_%s',path, num2str(a*180/pi), sprintf('%02d', L*100));
+    print(fname, '-depsc')
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%     fig = figure('Renderer', 'painters','Name','V_in vs V_out','NumberTitle','off', 'Position', [10 10 1300 600] );
-%     title(sprintf('V_{out} vs V_{in} (a=%s deg, L=%.2f H)', num2str(a*180/pi), L))
-%     figure()
-%     set(0,'DefaultLineLineWidth',1.2)
-% 
-%     numOfThyristors = length(thyristors);
-%     for i = 1:length(thyristors)
-%         subplot(2, numOfThyristors/2, i)
-%         
-%         curr = thyristors{i};
-%         plot(t_period,curr(1:length(t_period)))
-%         xlabel('Time (sec)');
-%         ylabel('Cuurent Amplitude (A)');
-%     end
+    fig = figure('Renderer', 'painters','Name','Thyristor_currents','NumberTitle','off', 'Position', [10 10 1300 600] );
+    title(sprintf('Thyristor_currents (a=%s deg, L=%.2f H)', num2str(a*180/pi), L))
+    set(0,'DefaultLineLineWidth',1.2)
+
+    numOfThyristors = length(thyristors);
+    for i = 1:length(thyristors)
+        subplot(2, numOfThyristors/2, i)
+
+        curr = thyristors{i};
+        plot(t_periods,curr(1:length(t_periods)))
+        xlabel('Time (sec)');
+        ylabel('Cuurent Amplitude (A)');
+    end
+
+    fname = sprintf('%s3_ThI_%s_%s',path, num2str(a*180/pi), sprintf('%02d', L*100));
+    print(fname, '-depsc')
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
